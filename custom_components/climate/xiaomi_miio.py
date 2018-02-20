4fa1f91998e31375148bbad261e6a27229519fce
Sebastian Muszynski <basti@linkt.de>
2018-02-20 10:38:23 +0100

    Swing mode attribute fixed. (Closes: #3)

--- custom_components/climate/xiaomi_miio.py
+++ custom_components/climate/xiaomi_miio.py
@@ -205,7 +205,7 @@ class XiaomiAirConditioningCompanion(ClimateDevice):
                 ATTR_AIR_CONDITION_MODEL: state.air_condition_model,
                 ATTR_AIR_CONDITION_POWER: state.air_condition_power,
                 ATTR_TEMPERATURE: state.temperature,
-                ATTR_SWING_MODE: state.swing_mode.name,
+                ATTR_SWING_MODE: state.swing_mode,
                 ATTR_FAN_SPEED: state.fan_speed.name,
                 ATTR_OPERATION_MODE: state.mode.name,
             }
