34e061b903f60cfcf5a84ad5a9c536a5ac5196b0
Sebastian Muszynski <basti@linkt.de>
2026-01-05 16:28:38 +0100

    Fix lint issues

--- custom_components/xiaomi_miio_airconditioningcompanion/climate.py
+++ custom_components/xiaomi_miio_airconditioningcompanion/climate.py
@@ -316,7 +316,9 @@ class XiaomiAirConditioningCompanion(ClimateEntity):
         from miio import DeviceException
 
         try:
-            result = await self.hass.async_add_executor_job(partial(func, *args, **kwargs))
+            result = await self.hass.async_add_executor_job(
+                partial(func, *args, **kwargs)
+            )
 
             _LOGGER.debug("Response received: %s", result)
 
@@ -561,7 +563,9 @@ class XiaomiAirConditioningCompanion(ClimateEntity):
                 _LOGGER.info(log_msg)
                 self.hass.async_create_task(
                     self.hass.services.async_call(
-                        "persistent_notification", "create", {"message": log_msg, "title": "Xiaomi Miio Remote"}
+                        "persistent_notification",
+                        "create",
+                        {"message": log_msg, "title": "Xiaomi Miio Remote"},
                     )
                 )
                 await self.hass.async_add_executor_job(self._device.learn_stop, slot)
@@ -573,7 +577,12 @@ class XiaomiAirConditioningCompanion(ClimateEntity):
         _LOGGER.error("Timeout. No infrared command captured")
         self.hass.async_create_task(
             self.hass.services.async_call(
-                "persistent_notification", "create", {"message": "Timeout. No infrared command captured", "title": "Xiaomi Miio Remote"}
+                "persistent_notification",
+                "create",
+                {
+                    "message": "Timeout. No infrared command captured",
+                    "title": "Xiaomi Miio Remote",
+                },
             )
         )
 
